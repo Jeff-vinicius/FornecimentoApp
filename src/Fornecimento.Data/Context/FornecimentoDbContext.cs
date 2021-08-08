791f5581cd5c9c086509c55eb5301664de7a2969
Jefferson Vinicius <jeffersonshady10@gmail.com>
2021-08-08 10:06:33 -0300

    Adicionar arquivos de projeto.

---
+++ src/Fornecimento.Data/Context/FornecimentoDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Fornecimento.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fornecimento.Data.Context
+{
+    public class FornecimentoDbContext : DbContext
+    {
+        public FornecimentoDbContext(DbContextOptions options) : base(options){ }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(FornecimentoDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ src/Fornecimento.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,36 @@
+﻿using Fornecimento.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fornecimento.Data.Mappings
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(p => p.Descricao)
+                .IsRequired()
+                .HasColumnType("varchar(1000)");
+
+            builder.Property(p => p.Imagem)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(p => p.Valor)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            builder.ToTable("Produtos");
+
+        }
+    }
+}
